650f235581fa7f0606de8f492b80861ba323bf2b
maisaa7x <maisaa7x@gmail.com>
2022-05-30 20:19:39 -0300

    primer cambio con bootstrap y navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BootNavBar from "./components/BootNavBar";
 function App() {
   return (
     <div>
-      <h1> Aguante la camada ahre</h1>
+      <h1 Aguante la camada ahre />
       <BootNavBar />
     </div>
   );
