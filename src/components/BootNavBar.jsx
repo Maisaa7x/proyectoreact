657db1645df54bfa19f64966159ea65875d7e9e6
maisaa7x <maisaa7x@gmail.com>
2022-05-30 16:38:10 -0300

    Agregue bootstrap

---
+++ src/components/BootNavBar.jsx
@@ -0,0 +1,12 @@
+function BootNavBar() {
+    return (
+      <div>
+        <h1> Esta es la navbar el componente
+            
+        </h1>
+        
+      </div>
+    );
+  }
+  
+  export default BootNavBar;
